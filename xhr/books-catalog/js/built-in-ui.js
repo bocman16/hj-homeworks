17c5fa6baeeb3e7baa5a661291d2daeaa9aec07b
bocman16 <bocman16@yandex.ru>
2019-01-12 16:26:18 +0300

    Выполнено задание book

--- xhr/books-catalog/js/built-in-ui.js
+++ xhr/books-catalog/js/built-in-ui.js
@@ -1,9 +1,9 @@
 /* Данный JS код */
 
 // Регулируем видимость карточки
-function toggleCardVisible () {
- document.getElementById('content').classList.toggle('hidden');
- document.getElementById('card').classList.toggle('hidden');
+function toggleCardVisible() {
+    document.getElementById('content').classList.toggle('hidden');
+    document.getElementById('card').classList.toggle('hidden');
 }
 
 
@@ -19,10 +19,32 @@ document.getElementById('content').addEventListener('click', (event) => {
     }
 
     if (target) {
-      toggleCardVisible();
-      document.getElementById('card-title').innerHTML = target.dataset.title;
-      document.getElementById('card-author').innerHTML = target.dataset.author;
-      document.getElementById('card-info').innerHTML = target.dataset.info;
-      document.getElementById('card-price').innerHTML = target.dataset.price;
+        toggleCardVisible();
+        document.getElementById('card-title').innerHTML = target.dataset.title;
+        document.getElementById('card-author').innerHTML = target.dataset.author;
+        document.getElementById('card-info').innerHTML = target.dataset.info;
+        document.getElementById('card-price').innerHTML = target.dataset.price;
     }
 });
+
+let xhr = new XMLHttpRequest();
+xhr.addEventListener("load", onLoad);
+xhr.open("GET", 'https://neto-api.herokuapp.com/book/', true);
+xhr.send();
+
+function onLoad() {
+    let book = JSON.parse(xhr.responseText);
+    let container = document.getElementById("content");
+    container.innerHTML -= `<li data-title data-author data-info data-price> <img src = ''></li>`;
+    for (let i = 0; i < book.length; i++) {
+        container.innerHTML += `<li data-title data-author data-info data-price> <img src = ''></li>`;
+        const bookList = document.querySelectorAll('ul#content li');
+        const imges = document.querySelectorAll('li img');
+        bookList[i].dataset.title = book[i].title;
+        bookList[i].dataset.author.name = book[i].author.name;
+        bookList[i].dataset.info = book[i].info;
+        bookList[i].dataset.price = book[i].price;
+        imges[i].src = book[i].cover.small;
+    };
+};
+
