93ec844db56ef3d5dc377abefbf3bfe5a66e3fd7
bocman16 <bocman16@yandex.ru>
2019-01-15 12:10:07 +0300

    Выполняю

---
+++ html-forms/feedback/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const form = document.getElementsByClassName('contentform') //Форма обратной связи доступна в документе по классу
+const buttonContact = document.getElementsByClassName('button-contact');
+const output = document.getElementById('output');
+output.classList.remove('hidden');
+
+alert(`lghlg`)
