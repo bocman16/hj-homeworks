e3ab009015b74d4148d9ff2b7b09af400eb18320
bocman16 <bocman16@yandex.ru>
2019-01-19 19:42:24 +0300

    Готово!

--- html-document-structure/todo-list/js/todo.js
+++ html-document-structure/todo-list/js/todo.js
@@ -1,13 +1,11 @@
 const todoList = document.querySelector('.todo-list');
-const doneAll = todoList.querySelectorAll('.done input');
+const doneAll = todoList.querySelectorAll('input');
 const done = todoList.querySelector('.done');
 const undone = todoList.querySelector('.undone');
-const undoneAll = todoList.querySelectorAll('.undone input');
 
 for (const button of doneAll) {
     button.addEventListener('click', itIsDone);
 };
-
 function itIsDone(event) {
     const tagNameParent = event.target.parentElement;
     const tagName = event.target;
@@ -17,21 +15,5 @@ function itIsDone(event) {
     } else {
         tagName.setAttribute('checked', '')
         done.appendChild(tagNameParent);
-    };
-};
-
-for (const button of undoneAll) {
-    button.addEventListener('click', jobFailed);
-};
-
-function jobFailed(enent) {
-    const tagNameParent = event.target.parentElement;
-    const tagName = event.target;
-    if (!tagName.hasAttribute('checked')) {
-        tagName.setAttribute('checked', '');
-        done.appendChild(tagNameParent);
-    } else {
-        tagName.removeAttribute('checked')
-        undone.appendChild(tagNameParent);
     };
 };
