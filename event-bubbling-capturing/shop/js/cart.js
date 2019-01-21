58bacfe59c48ddd9614bd37d1e2bc2a1d8ddb7ed
bocman16 <bocman16@yandex.ru>
2019-01-21 19:48:02 +0300

    Готово!

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const itemsList = document.querySelector('.items-list');
+itemsList.addEventListener('click', () => {
+    event.preventDefault();
+    const titlePrice = event.target.dataset;
+    const tagName = event.target.tagName;
+    if (tagName === "A") {
+        addToCart(titlePrice);
+    };
+});
