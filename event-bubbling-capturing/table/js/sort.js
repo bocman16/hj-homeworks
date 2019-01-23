2bf012dc6a3f940f76b61507d617e0749c52b159
bocman16 <bocman16@yandex.ru>
2019-01-23 18:45:23 +0300

    Готово!

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -2,10 +2,12 @@
 
 function handleTableClick(event) {
     const tagName = event.target.tagName;
-    if (tagName === 'TH') {
-        return;
+    if (tagName === 'TH' && event.target.dataset.dir === '1') {
+        event.target.dataset.dir = -1;
+
+    } else if (tagName === 'TH') {
+        event.target.dataset.dir = 1;
     }
-    event.target.dataset.dir = event.target.dataset.dir === '1' ? '-1' : '1';
     table.setAttribute('data-sort-by', event.target.dataset.propName);
     sortTable(event.target.dataset.propName, event.target.dataset.dir);
 };
