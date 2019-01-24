90b7cd45a3fbab4e4bee873cd0d997718d8559fc
bocman16 <bocman16@yandex.ru>
2019-01-24 19:12:17 +0300

    Готово!

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,22 @@
-'use strict';
+"use strict";
+
+const counter = document.querySelector("#counter");
+const button = document.querySelectorAll(".wrap-btns");
+counter.textContent = localStorage.counterData || "0";
+
+for (let click of button) {
+  click.addEventListener("click", count);
+}
+
+function count(event) {
+  const increment = document.querySelector("#increment");
+  const decrement = document.querySelector("#decrement");
+  if (event.target === increment) {
+    counter.textContent++;
+  } else if (event.target === decrement && counter.textContent > 0) {
+    counter.textContent--;
+  } else {
+    counter.textContent = 0;
+  }
+  localStorage.counterData = counter.textContent;
+}
