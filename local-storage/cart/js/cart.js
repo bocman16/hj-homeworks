a06cefab9fcd98e675ccb7adc657bf7c81b05eff
bocman16 <bocman16@yandex.ru>
2019-01-26 19:16:52 +0300

    Готово!

--- local-storage/cart/js/cart.js
+++ local-storage/cart/js/cart.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const colorSwatch = document.querySelector('#colorSwatch'); //Варианты цвета
+const sizeSwatch = document.querySelector('#sizeSwatch'); //Варианты размера
+const quickCart = document.querySelector('#quick-cart'); //Корзина 
+const quickCartPrice = document.querySelector('.quick-cart-price'); //Общая стоимость всех товаров
+// const header = document.querySelector('.header');
+// quickCart.addEventListener('submit', handleSubmitForm)
+
+// colorSwatch.addEventListener('click', event => {
+//     event.preventDefault();
+//     console.log(event.target)
+// })
+document.addEventListener('DOMContentLoaded', handleSubmitForm)
+
+function handleSubmitForm() {
+    Promise.all([
+            fetch('https://neto-api.herokuapp.com/cart/colors'),
+            fetch('https://neto-api.herokuapp.com/cart/sizes'),
+            fetch('https://neto-api.herokuapp.com/cart')
+        ]) // ]).then((data) => {
+        //     data.json()
+        // })
+        .then(([res1, res2, res3]) => {
+            res1.json(), res2.json(), res3.json()
+        .then((data) => {
+            console.log(data)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    })
+}
--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -1,1 +1,94 @@
-'use strict';
+"use strict";
+
+const baseUrl = 'https://neto-api.herokuapp.com/';
+
+const getUrl = string => `${baseUrl}${string}`;
+
+const mapValue = {
+    signIn: 'аворизован',
+    signUp: 'зарегистирирован'
+};
+
+const mapResultValue = Object.keys(mapValue).reduce((prev, cur) => {
+    prev[cur] = mapValue[cur];
+    console.log(prev)
+    return prev;
+}, {});
+
+
+const renderOutput = (form, response) => {
+    const output = form.querySelector('output');
+    output.value = !response.error ?
+        `Пользователь ${response.name} успешно ${mapResultValue[form.dataset.param]}` :
+        response.message;
+};
+
+const dataChange = {
+    render: renderOutput,
+    form: '',
+};
+
+const loadData = (url, data, paramsForm) =>
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': "application/json; charset=utf-8"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(res => {
+        const {
+            render,
+            form
+        } = paramsForm;
+        render(form, res);
+    })
+    .catch(error => console.log(error));
+
+const getFormData = form => {
+    const formData = new FormData(form);
+    return [...formData].reduce((prev, cur) => {
+        prev[cur[0]] = cur[1];
+        return prev;
+    }, {});
+};
+
+const handleSubmitForm = evt => {
+    evt.preventDefault();
+    const signin = document.querySelector('.sign-in-htm');
+    const form = evt.currentTarget;
+    if (signin === form) {
+        form.dataset.param = 'signIn'
+    } else {
+        form.dataset.param = 'signUp'
+    }
+    const paramUrl = getUrl([form.dataset.param]);
+    dataChange.form = form;
+    loadData(paramUrl, getFormData(form), dataChange);
+
+};
+
+const forms = document.querySelectorAll('form');
+[...forms].forEach(form => form.addEventListener('submit', handleSubmitForm));
+
+
+
+// async/await
+
+// const loadData = async (url, data, paramsForm) => {
+//     const obj = {
+//         method: 'POST',
+//         headers: {'Content-Type': "application/json; charset=utf-8"},
+//         body: ''
+//     };
+//     try {
+//         obj.body = JSON.stringify(data);
+//         const request = await fetch(url, obj);
+//         const response = await request.json();
+//         const {render, form} = paramsForm;
+//         render(form, response);
+//     } catch (e) {
+//         console.log(e.message);
+//     }
+// };
