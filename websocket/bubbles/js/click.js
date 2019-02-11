233c0cb6ebc3f0079095d7d5deef14db94dabae8
bocman16 <bocman16@yandex.ru>
2019-02-11 16:24:32 +0300

    Готово

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+connection.addEventListener('open', () => {
+    showBubbles(connection)
+})
+
+const getRelativeCoords = evt => {
+    const data = {x: evt.offsetX || evt.layerX, y: event.offsetY || evt.layerY}
+    connection.send(JSON.stringify(data));
+}
+document.addEventListener('click', getRelativeCoords)
+
