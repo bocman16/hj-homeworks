41bbf553cd451b6a729d64b26dfe5fed5867b1f9
bocman16 <bocman16@yandex.ru>
2019-02-12 11:59:13 +0300

    Задание 2 готово

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/counter');
+
+window.addEventListener('beforeunload', () => {
+    connection.onclose = function () {};
+    connection.close(1000, 'Работа закончена')
+})
+
+connection.addEventListener('message', event => {
+    const counter = document.querySelector('.counter');
+    const outputErrors = document.querySelector('output.errors');
+    let message = JSON.parse(event.data);
+    counter.textContent = message.connections;
+    outputErrors.textContent = message.errors;
+})
