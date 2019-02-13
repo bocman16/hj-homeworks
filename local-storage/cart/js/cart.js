d55ff8dad2e14defe9e61aa987c9d74eb14b3ff3
bocman16 <bocman16@yandex.ru>
2019-02-13 21:27:32 +0300

    Есть

--- local-storage/cart/js/cart.js
+++ local-storage/cart/js/cart.js
@@ -1,41 +1,149 @@
-'use strict';
+"use strict";
 
-const colorSwatch = document.querySelector('#colorSwatch'); //Варианты цвета
-const sizeSwatch = document.querySelector('#sizeSwatch'); //Варианты размера
-const quickCart = document.querySelector('#quick-cart'); //Корзина 
-const quickCartPrice = document.querySelector('.quick-cart-price'); //Общая стоимость всех товаров
-// const header = document.querySelector('.header');
-// quickCart.addEventListener('submit', handleSubmitForm)
+function loadColors(callBack) {
+  const colorSnippet = document.querySelector("#colorSwatch");
+  for (let color of callBack) {
+    colorSnippet.insertAdjacentHTML(
+      "beforeend",
+      `<div data-value="${color.type}" class="swatch-element color ${
+        color.type
+      }">
+  <div class="tooltip">${color.title}</div> <input quickbeam="color" id="${
+        color.type
+      }" type="radio" name="color" value="${color.type}">
+  <label for="${color.type}" style="border-color: red;">
+    <span style="background-color: ${color.code};"></span>
+    <img class="crossed-out" src="https://neto-api.herokuapp.com/hj/3.3/cart/soldout.png?10994296540668815886">
+  </label>
+</div>`
+    );
 
-// colorSwatch.addEventListener('click', event => {
-//     event.preventDefault();
-//     console.log(event.target)
-// })
-document.addEventListener('DOMContentLoaded', handleSubmitForm)
-
-function handleSubmitForm() {
-    Promise.all([
-            fetch('https://neto-api.herokuapp.com/cart/colors'),
-            fetch('https://neto-api.herokuapp.com/cart/sizes'),
-            fetch('https://neto-api.herokuapp.com/cart')
-        ])
-        .then(([res1, res2, res3]) => {
-            res1.json(), res2.json(), res3.json()
-        .then((data) => {
-            console.log(data)
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-    })
-}
-function loadData(url, callBack) {
-    fetch(url)
-      .then(res => res.json())
-      .then(res => callBack(res))
-      .catch(error => console.log(error))
+    if (color.isAvailable === false) {
+      let thisInput = colorSnippet.lastElementChild.querySelector("input");
+      thisInput.disabled = true;
+      colorSnippet.lastElementChild.classList.add("soldout");
+    } else {
+      colorSnippet.lastElementChild.classList.add("available");
+    }
   }
-  Promise.all([
-    loadData(url.color, loadColors),
-    loadData(url.size, loadSizes),
+  const div = colorSnippet.querySelector(`div`)
+  let colorRadio = colorSnippet.querySelector("input");
+  if(div.classList.contains('available')){
+    colorRadio.setAttribute('checked', '')
+  }else{ colorRadio.removeAttribute('checked') }
+}
+
+function loadSizes(callBack) {
+  const sizeSnippet = document.getElementById("sizeSwatch");
+  for (let size of callBack) {
+    sizeSnippet.insertAdjacentHTML(
+      "beforeend",
+      `<div data-value="${size.type}" class="swatch-element ${
+        size.type
+      } plain s">
+  <input id="${size.type}" type="radio" name="size" value="${size.type}">
+  <label for="${size.type}">
+    ${size.title}
+    <img class="crossed-out" src="https://neto-api.herokuapp.com/hj/3.3/cart/soldout.png?10994296540668815886">
+  </label>
+</div>`
+    );
+    if (size.isAvailable === false) {
+      let thisInput = sizeSnippet.lastElementChild.querySelector("input");
+      thisInput.disabled = true;
+      sizeSnippet.lastElementChild.classList.add("soldout"); 
+    } else {
+      sizeSnippet.lastElementChild.classList.add("available");
+    }
+  }
+  const div = sizeSnippet.querySelector('div')
+  let input = sizeSnippet.querySelector('input');
+  if(div.classList.contains('available')){
+    input.setAttribute('checked', '')
+  }else{ input.removeAttribute('checked') }
+}
+
+function loadData(url, callBack) {
+  fetch(url)
+    .then(res => res.json())
+    .then(res => callBack(res))
+    .catch(error => console.log(error));
+}
+
+Promise.all([
+  loadData("https://neto-api.herokuapp.com/cart/colors", loadColors),
+  loadData("https://neto-api.herokuapp.com/cart/sizes", loadSizes)
 ]);
+
+const loadCart = cart => {
+  console.log(cart);
+  const quickCart = document.querySelector("#quick-cart");
+  const quantity = cart[0].quantity ? cart[0].quantity : 1;
+  if (cart.length === 0) {
+    quickCart.innerHTML = "";
+    return;
+  }
+  let sum = quantity * cart[0].price;
+  quickCart.innerHTML = "";
+  quickCart.insertAdjacentHTML(
+    "beforeend",
+    `<div class="quick-cart-product quick-cart-product-static" id="${
+      cart[0].id
+    }" style="opacity: 1;">
+<div class="quick-cart-product-wrap"><img src="${cart[0].pic}" title="${
+      cart[0].title
+    }">
+  <span class="s1" style="background-color: #000; opacity: .5">${sum}</span><span class="s2"></span>
+</div>
+<span class="count hide fadeUp" id="${cart[0].id}">${quantity}</span>
+<span class="quick-cart-product-remove remove" data-id="${cart[0].id}"></span>
+</div>`)
+  quickCart.insertAdjacentHTML("beforeend", `<a id="quick-cart-pay" quickbeam="cart-pay" class="cart-ico open">
+ <span>
+   <strong class="quick-cart-text">Оформить заказ<br></strong>
+   <span id="quick-cart-price">$${sum}</span>
+ </span>
+</a>`);
+  const remove = document.querySelector(".remove");
+  remove.addEventListener("click", handleSubmitForm);
+};
+
+const sendRequest = (data, url) => {
+  fetch(url, {
+      body: data,
+      method: "POST"
+    })
+    .then(res => res.json())
+    .then(res => loadCart(res))
+    .catch(error => console.log(error));
+};
+
+/////////
+const getFormData = form => {
+  let id = form.dataset.productId;
+  const formData = new FormData(form);
+  formData.append("productId", id);
+  return formData;
+};
+
+const handleSubmitForm = evt => {
+  evt.preventDefault();
+  const form = evt.currentTarget
+  let url = `https://neto-api.herokuapp.com/cart`;
+  if (evt.target.classList.contains("remove")) {
+    url = `https://neto-api.herokuapp.com/cart/remove`;
+  }
+  sendRequest(getFormData(form), url);
+};
+const AddToCartForm = document.querySelector("#AddToCartForm");
+AddToCartForm.addEventListener("submit", handleSubmitForm);
+
+//////////////////
+const updateLocalStorage = evt => {
+  if (evt.target.type !== "radio") {
+    return;
+  }
+  localStorage.setItem(`${evt.target.name}`, `${evt.target.value}`);
+};
+
+document.addEventListener("click", updateLocalStorage);
--- websocket/chat/js/chat.js
+++ websocket/chat/js/chat.js
@@ -1,1 +1,73 @@
-'use strict';
+"use strict";
+
+const chat = document.querySelector(".chat");
+const messagesContent = chat.querySelector(".messages-content");
+const messageSubmit = chat.querySelector(".message-submit");
+const messagesTemplates = chat.querySelector(".messages-templates");
+const messageStatus = messagesTemplates.querySelector(".message-status");
+const messageText = messageStatus.querySelector(".message-text");
+const chatStatus = chat.querySelector(".chat-status");
+
+const connection = new WebSocket("wss://neto-api.herokuapp.com/chat");
+
+connection.addEventListener("open", () => {
+  chatStatus.textContent = chatStatus.dataset.online;
+  messageSubmit.disabled = false;
+  messagesContent.appendChild(messageStatus);
+  messageText.textContent = "«Пользователь появился в сети»";
+});
+
+window.addEventListener("beforeunload", () => {
+  connection.onclose = () => {
+    chatStatus.textContent = chatStatus.dataset.offline;
+    messageSubmit.disabled = true;
+    messagesContent.appendChild(messageStatus);
+    messageText.textContent = messageText.value;
+  };
+  connection.close();
+});
+
+connection.addEventListener("message", event => {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const divMessage = messagesTemplates.querySelectorAll(".message");
+  const divClone = divMessage[1].cloneNode(true);
+  const text = divClone.querySelector(".message-text");
+  const time = divClone.querySelector(".timestamp");
+  time.textContent = `${hours}:${minutes}`;
+  messagesContent.appendChild(divClone);
+  text.scrollIntoView(top);
+  if (event.data === "...") {
+    messagesContent.appendChild(divMessage[0]);
+    text.textContent = "собеседник сейчас печатает сообщение";
+  } else {
+    const remomeDiv = messagesContent.querySelectorAll(".loading");
+    if (remomeDiv.length >= 1) {
+      messagesContent.removeChild(divMessage[0]);
+    }
+    text.textContent = event.data;
+  }
+});
+const handleSubmitForm = evt => {
+  evt.preventDefault();
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const messageInput = chat.querySelector(".message-input");
+  const messagePersonal = messagesTemplates.querySelector(".message-personal");
+  const messagText = messagePersonal.querySelector(".message-text");
+  const time = messagePersonal.querySelector(".timestamp");
+  connection.send(messageInput.value);
+  time.textContent = `${hours}:${minutes}`;
+  messagText.textContent = messageInput.value;
+  const div = messagePersonal.cloneNode(true);
+  messagesContent.appendChild(div);
+  div.scrollIntoView(top);
+  messageInput.value = "";
+};
+
+const messageBox = chat.querySelectorAll(".message-box");
+for (let form of messageBox) {
+  form.addEventListener("submit", handleSubmitForm);
+}
