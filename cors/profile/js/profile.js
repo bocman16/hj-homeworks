7c33b1d7dfd8e31bbd9d58e7527912e76663f196
bocman16 <bocman16@yandex.ru>
2019-02-17 20:21:49 +0300

    Готово!

--- cors/profile/js/profile.js
+++ cors/profile/js/profile.js
@@ -1,37 +1,37 @@
 'use strict';
 
-function userValue(data){
-    const content = document.querySelector('.content');
-    const name = content.querySelector('[data-name]')
-    const description = content.querySelector('[data-description]')
-    const pic = content.querySelector('[data-pic]')
-    const position = content.querySelector('[data-position]')
-    const technologies = content.querySelector('[data-technologies]')
-    const following = content.querySelector('[data-following]')
-    name.textContent = data.name;
+const content = document.querySelector('.content');
 
-    console.log(data)
-}
-
-
+function userValue(data) {
+  content.querySelector('[data-name]').textContent = data.name;
+  content.querySelector('[data-description]').textContent = data.description;
+  content.querySelector('[data-pic]').src = data.pic;
+  content.querySelector('[data-position]').textContent = data.position;
+  let id = `${data.id}/technologies`;
+  loadData(`https://neto-api.herokuapp.com/profile/` + `${id}`).then(technologiesUsed);
+};
 
 function randName() {
-    let callbackName = "cb" + String(Math.random()).slice(-6);
-    return callbackName;
-  }
-  
-  function loadData(url) {
-    const functionName = randName();
-    return new Promise((done, fail) => {
-      window[functionName] = done;
-      const script = document.createElement("script");
-      script.src = `${url}?jsonp=${functionName}`;
-      document.body.appendChild(script);
-    });
-  }
-  loadData("https://neto-api.herokuapp.com/profile/me").then(userValue);
-  
-  
-//  function technologiesUsed(){
+  let callbackName = "cb" + String(Math.random()).slice(-6);
+  return callbackName;
+};
 
-//  }
+function loadData(url) {
+  const functionName = randName();
+  return new Promise((done, fail) => {
+    window[functionName] = done;
+    const script = document.createElement("script");
+    script.src = `${url}?jsonp=${functionName}`;
+    document.body.appendChild(script);
+  });
+};
+loadData("https://neto-api.herokuapp.com/profile/me").then(userValue);
+
+
+function technologiesUsed(id) {
+  const technologies = content.querySelector('[data-technologies]');
+  for (let i = 0; i < id.length; i++) {
+    technologies.insertAdjacentHTML(`beforeEnd`, `<span class="devicons devicons-${id[i]}"></span>`);
+  }
+  content.style.display = 'initial';
+};
