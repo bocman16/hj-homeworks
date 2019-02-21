eb87ce3549959915842f9f4b1a9c87f114a70793
bocman16 <bocman16@yandex.ru>
2019-02-21 16:28:32 +0300

    Задание 2 Готово

--- canvas/draw/js/draw.js
+++ canvas/draw/js/draw.js
@@ -1,126 +1,101 @@
-'use strict';
+"use strict";
 
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-ctx.canvas.width = window.innerWidth;
-ctx.canvas.height = window.innerHeight;
+const convas = document.querySelector("#draw");
+const ctx = convas.getContext("2d");
+const PI = Math.PI;
+let draw = false; // Статус рисования
 
-const BRUSH_RADIUS = 6;
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
 
-let curves = [];
-let undone = [];
-let drawing = false;
-let weird = false;
-let needsRepaint = false;
+document.body.style.overflow = "hidden";
 
-// curves and figures
-function circle(point) {
-  ctx.beginPath();
-  ctx.arc(...point, BRUSH_RADIUS / 2, 0, 2 * Math.PI);
-  ctx.fill();
-}
+window.addEventListener("resize", () => {
+  sizeDraw();
+});
 
-function smoothCurveBetween (p1, p2) {
-  // Bezier control point
-  const cp = p1.map((coord, idx) => (coord + p2[idx]) / 2);
-  ctx.quadraticCurveTo(...p1, ...cp);
-}
+// Размеры холста относительно экрана
+const sizeDraw = () => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
 
-function smoothCurve(points) {
-  ctx.beginPath();
-  ctx.lineWidth = BRUSH_RADIUS;
-  ctx.lineJoin = 'round';
-  ctx.lineCap = 'round';
+  convas.setAttribute("width", width);
+  convas.setAttribute("height", height);
+  ctx.clearRect(0, 0, width, height);
+};
+sizeDraw();
 
-  ctx.moveTo(...points[0]);
 
-  for(let i = 1; i < points.length - 1; i++) {
-    smoothCurveBetween(points[i], points[i + 1]);
+convas.addEventListener("mousedown", e => {
+  draw = true;
+});
+
+convas.addEventListener("mouseup", () => {
+  draw = false;
+});
+
+convas.addEventListener("mouseleave", () => {
+  draw = false;
+});
+
+convas.addEventListener("mousemove", e => {
+  if (draw) {
+    const point = [e.offsetX, e.offsetY];
+    circle(point, lineSize(), hue(e));
   }
+});
 
-  ctx.stroke();
-}
+convas.addEventListener("dblclick", () => {
+  ctx.clearRect(0, 0, convas.width, convas.height);
+});
 
-// events
-function makePoint(x, y, reflect = false) {
-  return  reflect ? [y, x] : [x, y];
+// Рисование точки
+const circle = (point, size, hue) => {
+  ctx.beginPath();
+  ctx.fillStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.arc(...point, size, 0, 2 * PI);
+  ctx.fill();
 };
 
+// Изменение цвета
+const changeHue = () => {
+  const minHue = 0;
+  const maxHue = 359;
+  let currentHue = 0;
 
-doodle.addEventListener("mousedown", (evt) => {
-  drawing = true;
-  weird = evt.shiftKey; // press shift to make things weird =)
-  undone = []; // reset the undone stack
+  return event => {
+    event.shiftKey ? currentHue-- : currentHue++;
+    
+    if (currentHue < minHue) {
+      currentHue = maxHue;
+    }
+    if (currentHue > maxHue) {
+      currentHue = minHue;
+    }
+    return currentHue;
+  };
+};
+const hue = changeHue(); // Цвет
 
-  const curve = []; // create a new curve
+// Изменение размера
+const changeLineSize = () => {
+  const lineMinWidth = 5;
+  const lineMaxWidth = 100;
+  let currentLineWidth = lineMinWidth;
+  let inc = true;
 
-  curve.push(makePoint(evt.offsetX, evt.offsetY, weird)); // add a new point
-  curves.push(curve); // add the curve to the array of curves
-  needsRepaint = true;
-});
+  return () => {
+    if (currentLineWidth < lineMaxWidth && inc) {
+      return currentLineWidth++;
+    } else {
+      inc = false;
+    }
 
-doodle.addEventListener("mouseup", (evt) => {
-  drawing = false;
-});
-
-doodle.addEventListener("mouseleave", (evt) => {
-  drawing = false;
-});
-
-doodle.addEventListener("mousemove", (evt) => {
-  if (drawing) {
-    // add a point
-    const point = makePoint(evt.offsetX, evt.offsetY, weird)
-    curves[curves.length - 1].push(point);
-    needsRepaint = true;
-  }
-});
-
-undoBtn.addEventListener("click", (evt) => {
-  if (curves.length > 0) {
-    undone.push(curves.pop());
-  }
-
-  needsRepaint = true;
-});
-
-redoBtn.addEventListener("click", (evt) => {
-  if (undone.length > 0) {
-    curves.push(undone.pop());
-  }
-
-  needsRepaint = true;
-});
-
-clearBtn.addEventListener("click", (evt) => {
-  curves = [];
-  undone = [];
-
-  needsRepaint = true;
-});
-
-// rendering
-function repaint () {
-  // clear before repainting
-  ctx.clearRect(0, 0, doodle.width, doodle.height);
-
-  curves
-    .forEach((curve) => {
-      // first...
-      circle(curve[0]);
-
-      // the body is compraised of lines
-      smoothCurve(curve);
-    });
-}
-
-function tick () {
-  if(needsRepaint) {
-    repaint();
-    needsRepaint = false;
-  }
-
-  window.requestAnimationFrame(tick);
-}
-
-tick();
+    if (currentLineWidth > lineMinWidth && !inc) {
+      return currentLineWidth--;
+    } else {
+      inc = true;
+    }
+  };
+};
+const lineSize = changeLineSize(); // Размер
