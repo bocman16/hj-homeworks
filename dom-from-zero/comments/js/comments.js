3d68bf6be664dc7f239780ece2c89efb9e474e13
bocman16 <bocman16@yandex.ru>
2019-02-24 16:08:37 +0300

    Задание 1 готово!

--- dom-from-zero/comments/js/comments.js
+++ dom-from-zero/comments/js/comments.js
@@ -53,23 +53,7 @@ function createProductNode(comment) {
     ])
   ]);
 }
-// `<div class="comment-wrap">
-// //     <div class="photo" title="${comment.author.name}">
-// //       <div class="avatar" style="background-image: url('${comment.author.pic}')"></div>
-// //     </div>
-// //     <div class="comment-block">
-// //       <p class="comment-text">
-// //         ${comment.text.split('\n').join('<br>')}
-// //       </p>
-// //       <div class="bottom-comment">
-// //         <div class="comment-date">${new Date(comment.date).toLocaleString('ru-Ru')}</div>
-// //         <ul class="comment-actions">
-// //           <li class="complain">Пожаловаться</li>
-// //           <li class="reply">Ответить</li>
-// //         </ul>
-// //       </div>
-// //     </div>
-// //   </div>`
+
 
 function showComments(list) {
   const productNodes = list.map(createProductNode),
