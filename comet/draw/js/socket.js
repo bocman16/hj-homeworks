91d66589e34f1c2aa48c6edba824bdbb622b3210
bocman16 <bocman16@yandex.ru>
2019-03-04 15:35:18 +0300

    Задание 2 Готово!

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const ws = new WebSocket("wss://neto-api.herokuapp.com/draw");
+
+const editor = window.editor;
+editor.addEventListener('update', e => {
+    const canvas = document.querySelector('#canvas');
+    canvas.toBlob(blob => {
+        ws.send(blob)
+    })
+})
