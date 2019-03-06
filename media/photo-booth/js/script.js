f52c3934d47a3a689c2ef844aa93da90e0fe4f36
bocman16 <bocman16@yandex.ru>
2019-03-06 14:58:59 +0300

    Готово!

--- media/photo-booth/js/script.js
+++ media/photo-booth/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const app = document.querySelector('.app');
+const video = document.createElement('video');
+app.appendChild(video);
+
+
+navigator.mediaDevices
+.getUserMedia({video: true, audio: false})
+.then((stream) => {
+   const video = document.querySelector('video');
+    video.src = URL.createObjectURL(stream);
+    video.play()
+    })
+.catch(err => console.warn('oh noes'));
+
+
+
