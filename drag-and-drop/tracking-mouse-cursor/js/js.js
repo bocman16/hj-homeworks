bf4e9316bff31dedbdd484ea331d1ab8c9e487af
bocman16 <bocman16@yandex.ru>
2019-03-13 15:25:36 +0300

    Готово

--- drag-and-drop/tracking-mouse-cursor/js/js.js
+++ drag-and-drop/tracking-mouse-cursor/js/js.js
@@ -1,37 +1,20 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', updateEyePos);
-document.addEventListener('mousemove', onMouseMove);
-document.addEventListener('scroll', updateEyePos);
 
+document.addEventListener("DOMContentLoaded", function () {
+    let X = 0;
+    let Y = 0;
 
-const eye = document.querySelector('.cat_eye_left');
-let eyePos;
-let eyeOtherPos;
+    function move() {
+        document.querySelector('.cat_eye_left').style.left = X + 'px';
+        document.querySelector('.cat_eye_left').style.top = Y + 'px';
+    }
+    document.addEventListener("mousemove", function (e) {
+        X = e.clientX;
+        Y = e.clientY;
+    }, false);
 
-function updateEyePos() {
-  eyePos = eye.getBoundingClientRect();
-  eyeOtherPos = {x: (eyePos.left + eyePos.right) / 2, y: (eyePos.top + eyePos.bottom) / 2};
- 
-}
+    setInterval(move, 100);
+});
 
-
-function onMouseMove(event) {
-  updateEyeParams({x: event.clientX, y: event.clientY}, eyeOtherPos);
-}
-
-function updateEyeParams(mouseCoords, eyeCenterCoords) {
-    const offsetX = (mouseCoords.x - eyeCenterCoords.x) /
-      ((mouseCoords.x > eyeCenterCoords.x) ? window.innerWidth - eyeCenterCoords.x : eyeCenterCoords.x);
-  
-    const offsetY = (mouseCoords.y - eyeCenterCoords.y) /
-      ((mouseCoords.y > eyeCenterCoords.y) ? window.innerHeight - eyeCenterCoords.y : eyeCenterCoords.y);
-  
-    const pupil = document.querySelector('.cat_eye_left');
-    pupil.style.setProperty('--pupil-x', getResult(offsetX));
-    pupil.style.setProperty('--pupil-y', getResult(offsetY));
-  }
-  
-  
-  function getResult(percent) {
-    return `${Math.round(percent * 30)}px`; }
+console.log('sss')
