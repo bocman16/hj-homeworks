50c782809d3b62dcbfb01bb4fb33c46b915f3fd7
bocman16 <bocman16@yandex.ru>
2019-03-16 13:36:36 +0300

    Задание 2 Готово

--- drag-and-drop/tracking-mouse-cursor/js/js.js
+++ drag-and-drop/tracking-mouse-cursor/js/js.js
@@ -1,20 +1,41 @@
-'use strict';
+"use strict";
+document.addEventListener('mousemove', event => moveEyes(event))
+
+function Eye(className) {
+  this.eye = document.querySelector(`${className}`);
+  this.box = this.eye.getBoundingClientRect();
+  this.centrePoint =  window.getComputedStyle(this.eye).transformOrigin;
+  this.centers = this.centrePoint.split(" ");
+}
+
+const le = new Eye('.cat_position_for_left_eye')
+const re = new Eye('.cat_position_for_right_eye')
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    let X = 0;
-    let Y = 0;
+const moveEyes = throttle((event) =>{
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+  rotateEye(le, mouseX, mouseY);
+  rotateEye(re, mouseX, mouseY);
+})
 
-    function move() {
-        document.querySelector('.cat_eye_left').style.left = X + 'px';
-        document.querySelector('.cat_eye_left').style.top = Y + 'px';
+function rotateEye(eye, mouseX, mouseY) {
+  const centerY = eye.box.top + parseInt(eye.centers[1]) - window.pageYOffset;
+  const centerX = eye.box.left + parseInt(eye.centers[0]) - window.pageXOffset;
+  const radians = Math.atan2(mouseX - centerX, mouseY - centerY);
+  const degrees = (radians * (180 / Math.PI) * -1) + 45;
+  eye.eye.style.transform = 'rotate('+degrees+'deg)';
+}
+
+function throttle(callback) {
+  let isWaiting = false;
+  return function () {
+    if (!isWaiting) {
+      callback.apply(this, arguments);
+      isWaiting = true;
+      requestAnimationFrame(() => {
+        isWaiting = false;
+      });
     }
-    document.addEventListener("mousemove", function (e) {
-        X = e.clientX;
-        Y = e.clientY;
-    }, false);
-
-    setInterval(move, 100);
-});
-
-console.log('sss')
+  };
+}
