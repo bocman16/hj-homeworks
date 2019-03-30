f1af4a8ac71d0bc6ec726e67bb6422fe60266abd
bocman16 <bocman16@yandex.ru>
2019-03-30 11:44:46 +0300

    ок

--- drag-and-drop/trash-bin/JS/js.js
+++ drag-and-drop/trash-bin/JS/js.js
@@ -1,46 +1,44 @@
-"use script";
+'use script';
 
 let movedPiece = null;
 let shiftX = 0;
 let shiftY = 0;
 
-document.addEventListener("mousedown", event => {
-  if (event.target.classList.contains("logo")) {
+document.addEventListener('mousedown', (event) => {
+  if (event.target.classList.contains('logo')) {
     movedPiece = event.target;
     // const bounds = event.target.getBoundingClientRect();
-    shiftX =
-      event.pageX -
-      event.target.getBoundingClientRect().left -
-      window.pageXOffset;
-    shiftY =
-      event.pageY -
-      event.target.getBoundingClientRect().top -
-      window.pageYOffset;
-    movedPiece.style.left = event.pageX - shiftX + "px";
-    movedPiece.style.top = event.pageY - shiftY + "px";
+    shiftX =      event.pageX
+      - event.target.getBoundingClientRect().left
+      - window.pageXOffset;
+    shiftY =      event.pageY
+      - event.target.getBoundingClientRect().top
+      - window.pageYOffset;
+    movedPiece.style.left = `${event.pageX - shiftX}px`;
+    movedPiece.style.top = `${event.pageY - shiftY}px`;
   }
 });
 
-document.addEventListener("mousemove", event => {
+document.addEventListener('mousemove', (event) => {
   if (movedPiece) {
     // Предотвращаем выделение текста
     event.preventDefault();
-    movedPiece.style.left = event.pageX - shiftX + "px";
-    movedPiece.style.top = event.pageY - shiftY + "px";
-    movedPiece.classList.add("moving");
+    movedPiece.style.left = `${event.pageX - shiftX}px`;
+    movedPiece.style.top = `${event.pageY - shiftY}px`;
+    movedPiece.classList.add('moving');
   }
 });
 
-document.addEventListener("mouseup", event => {
+document.addEventListener('mouseup', (event) => {
   if (movedPiece) {
     const trash_bin = document
       .elementFromPoint(event.clientX, event.clientY)
-      .closest("#trash_bin");
+      .closest('#trash_bin');
     if (trash_bin) {
-      movedPiece.classList.remove("moving");
-      movedPiece.style.display = "none";
+      movedPiece.classList.remove('moving');
+      movedPiece.style.display = 'none';
     } else {
-      movedPiece.classList.remove("moving");
+      movedPiece.classList.remove('moving');
       movedPiece = null;
     }
   }
